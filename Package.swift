d6f4cda1ac645042e98cfe3c5e4d5afac7bed81b
marius <gabrielmarius08@gmail.com>
2024-08-19 09:05:07 +0300

    add

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "PDFReader"
+    name: "PDFReader",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "PDFReader",
+            targets: ["PDFReader"]
+            ),
+    ],
+    targets: [
+        .target(
+            name: "PDFReader"
+        )
+    ]
 )
